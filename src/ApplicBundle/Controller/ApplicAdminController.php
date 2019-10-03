983aa22afd4c8c2be43d47041995be44d6563fef
Artem <temicska99@mail.ru>
2019-10-04 00:11:53 +0300

    Контроллер для заявок трудозатраты: 15 минут

---
+++ src/ApplicBundle/Controller/ApplicAdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ApplicBundle\Controller;
+
+use AppBundle\Interfaces\MyController;
+use AppBundle\Interfaces\MyJsonResponse;
+use ApplicBundle\Entity\Applic;
+use ApplicBundle\Entity\VocabApplicStatus;
+
+class ApplicAdminController extends MyController
+{
+    /**
+     * Экшен: Получение списка всех заявок
+     */
+    public function getAllApplicsAction()
+    {
+        $applicRepository = $this->getEntityManager()->getRepository(Applic::class);
+        /** @var Applic $applic */
+        $applics = $applicRepository->findAll();
+
+
+    }
+
+    /**
+     * Экшен: Обновление заявки
+     */
+    public function updateApplicStatus($applicId, $statusId)
+    {
+        if (!($applicId > 0) || !($statusId > 0)) {
+            return false;
+        }
+
+        $statusRepo = $this->getEntityManager()->getRepository(VocabApplicStatus::class);
+        /** @var VocabApplicStatus $status */
+        $status = $statusRepo->find($statusId);
+
+        if ($status->getId() != $statusId) {
+            return false;
+        }
+
+        $applicRepository = $this->getEntityManager()->getRepository(Applic::class);
+        /** @var Applic $applic */
+        $applic = $applicRepository->find($applicId);
+
+        $applic->setStatus($status);
+
+        return new MyJsonResponse(true, 'Статус у заявки #'. $applic->getId() .' изменен!');
+    }
+}
