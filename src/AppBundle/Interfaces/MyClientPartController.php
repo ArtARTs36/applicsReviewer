c58bcd24c5ae35e874d777417e7d1c37012b4115
Artem <temicska99@mail.ru>
2019-11-02 15:13:07 +0300

    - Динамические блоки дел

--- src/AppBundle/Interfaces/MyClientPartController.php
+++ src/AppBundle/Interfaces/MyClientPartController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Interfaces;
 
+use AppBundle\Entity\Work;
 use ApplicBundle\Entity\OfferDocument;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,9 +15,17 @@ class MyClientPartController extends MyController
         return $offerRepo->findBy([], ['id' => 'desc'], 4);
     }
 
+    private function getAllWorks()
+    {
+        $repo = $this->getEntityManager()->getRepository(Work::class);
+
+        return $repo->findBy([], ['id' => 'desc'], 4);
+    }
+
     public function render($view, array $parameters = [], Response $response = null)
     {
         $parameters['offerDocuments'] = $this->getListOfferDocuments();
+        $parameters['worksBlock'] = $this->getAllWorks();
 
         return parent::render($view, $parameters, $response);
     }
